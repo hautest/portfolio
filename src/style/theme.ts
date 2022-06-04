b39be650bff8af14e598a7ece21fe528b932e159
LeeSeungHun <dos07008@naver.com>
2022-06-04 21:05:51 +0900

    feat:style theme

---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+const colors = {
+  black: "#000000",
+  white: "#FFFFFF",
+  background: "E6E7E8",
+};
+
+export const theme = {
+  colors,
+};
+
+export type ThemeColorType = keyof typeof colors;
